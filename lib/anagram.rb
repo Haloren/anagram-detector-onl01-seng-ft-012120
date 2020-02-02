8c5ec8071a7aee82e1ec184a5f5a420f6480b576
Kyle McKenna <mckennakyler@gmail.com>
2020-02-02 03:49:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,17 +7,19 @@ class Anagram
   end   
   
   def match(word_array) 
-  # array1 = %w(@word)
-  # array2 = %w(word) 
-  #   array1.sort == array2.sort    
-  # end       
+  array1 = %w(@word)
+  array2 = %w(word) 
+    array1.sort == array2.sort    
+  end       
     
   # compare each word of each array @word compared to (==) word
-    word_array.select do |word|  # .select method   group = %w(item item item)
-      @word.split("").sort == word.split("").sort 
+ 
+    # word_array.select do |word|  # .select method   group = %w(item item item)
+    #   @word.split("").sort == word.split("").sort 
+    # end 
+    
   # split @word & word into arrays and sort them to compare 
   # arrays are = if contain same elements in same order (.sort)
-    end
   end 
   
 end 
