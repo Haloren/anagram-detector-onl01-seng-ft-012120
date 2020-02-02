73312aaa137db297a5ea35d50084601378b23a0e
Kyle McKenna <mckennakyler@gmail.com>
2020-02-02 03:49:05 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word) #initialize a word 
+    @word = word
+  end   
+  
+  def match(word_array) 
+  # array1 = %w(@word)
+  # array2 = %w(word) 
+  #   array1.sort == array2.sort    
+  # end       
+    
+  # compare each word of each array @word compared to (==) word
+    word_array.select do |word|  # .select method   group = %w(item item item)
+      @word.split("").sort == word.split("").sort 
+  # split @word & word into arrays and sort them to compare 
+  # arrays are = if contain same elements in same order (.sort)
+    end
+  end 
+  
+end 
